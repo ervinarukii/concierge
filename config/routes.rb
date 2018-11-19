ee11cfe381762eb89abceb81f167d9feb7ddfe1e
david <david@clappingape.com>
2018-11-19 23:19:34 +0700

    update blog index

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   root 'blogs#index'
   get 'blogs/index'
+  get 'posts/blogs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
